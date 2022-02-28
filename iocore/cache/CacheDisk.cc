7a7a7f2537db04041bf88548b5eafef9b0b88ad4
Piotr Sieduszewski <piotr.sieduszewski@intel.com>
2022-02-28 11:30:10 +0100

    hugepages !storage.config: Add extra 0x2000 bytes to file size for align to hugepages!
    ipmctl create -goal MemoryMode=0 && reboot
    ndctl create-namespace --mode=devdax --map=mem
    daxctl create-device -u || for D in /dev/dax* ; do daxctl reconfigure-device ${D##*/} --mode=system-ram; done
    for NR in /sys/devices/system/node/node[23]/hugepages/hugepages-2048kB/nr_hugepages ; do echo 1048576 >$NR ; cat $NR ; done
    autoreconf -if
    ./configure --enable-mmap
    
    !# for D in /dev/dax* ; do daxctl destroy-device ${D##*/} -f ; done

--- iocore/cache/CacheDisk.cc
+++ iocore/cache/CacheDisk.cc
@@ -62,7 +62,10 @@ CacheDisk::open(char *s, off_t blocks, off_t askip, int ahw_sector_size, int fil
   path           = ats_strdup(s);
   hw_sector_size = ahw_sector_size;
 #if TS_USE_MMAP
-  fd = mmap(0, blocks * STORE_BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fildes, 0);
+  fd = mmap(0, blocks * STORE_BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_HUGETLB | MAP_ANONYMOUS , fildes, 0);
+  if(MAP_FAILED==fd){
+      fprintf(stderr," MAP_FAILED %lx MAP_HUGETLB MAP_ANONYMOUS\n", blocks * STORE_BLOCK_SIZE);
+      fd = mmap(0, blocks * STORE_BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE , fildes, 0);}
   ink_assert(MAP_FAILED != fd);
 #else
   fd = fildes;
